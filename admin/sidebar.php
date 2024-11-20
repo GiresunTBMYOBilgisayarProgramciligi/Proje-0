0fa1bd62b7fc1d21db6ed0179fc210b714864a08
Samet <samet@gencbilisim.net>
2024-11-20 18:45:16 +0300

    sidebar güncellendi

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -23,7 +23,8 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
-                <li class="nav-item menu-open"> <!-- menu-open sınıfı dropdown menünün açık olmasını sağlıyor-->
+                <!-- Kullanıcı Menusü -->
+                <li class="nav-item <?= (strpos($page, 'user') !== false) ? 'menu-open' : ''; ?>""> <!-- menu-open sınıfı dropdown menünün açık olmasını sağlıyor-->
                     <a href="#" class="nav-link <?= (strpos($page, 'user') !== false) ? 'active' : ''; ?>"> <!-- sayfa user kelimesini içeriyorsa ektif olur-->
                         <i class="nav-icon fas fa-users"></i>
                         <p>
@@ -41,11 +42,37 @@
                         <li class="nav-item">
                             <a href="index.php?p=new_user" class="nav-link <?= ($page == 'new_user') ? 'active' : ''; ?>">
                                 <i class="fa fa-user-plus nav-icon"></i>
-                                <p>Yeni Ekle</p>
+                                <p>Kullanıcı Ekle</p>
                             </a>
                         </li>
                     </ul>
                 </li>
+                <!-- /Kullanıcı Menusü -->
+                <!-- Haber Menusü -->
+                <li class="nav-item <?= (strpos($page, 'news') !== false) ? 'menu-open' : ''; ?>"> <!-- menu-open sınıfı dropdown menünün açık olmasını sağlıyor-->
+                    <a href="#" class="nav-link <?= (strpos($page, 'news') !== false) ? 'active' : ''; ?>"> <!-- sayfa user kelimesini içeriyorsa ektif olur-->
+                        <i class="nav-icon fas fa-newspaper"></i>
+                        <p>
+                            Haber İşlemleri
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="index.php?p=news_list" class="nav-link <?= ($page == 'news_list') ? 'active' : ''; ?>">
+                                <i class="fas fa-clipboard-list nav-icon"></i>
+                                <p>Haber Listesi</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="index.php?p=new_news" class="nav-link <?= ($page == 'new_news') ? 'active' : ''; ?>">
+                                <i class="fa fa-plus nav-icon"></i>
+                                <p>Haber Ekle</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- /Haber Menusü -->
                 <li class="nav-item">
                     <a href="index.php?p=simple_link" class="nav-link <?= ($page == 'simple_link') ? 'active' : ''; ?>">
                         <i class="nav-icon fas fa-th"></i>
