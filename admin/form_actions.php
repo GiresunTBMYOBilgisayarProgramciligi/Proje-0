b215ab36e5de52c5108e7edcaf88b4271e082c2c
sametatabasch <sametatabasch@gmail.com>
2024-11-29 18:07:14 +0300

    form_actions.php dosyasında fonksiyonlar oluşturuldu

--- admin/form_actions.php
+++ admin/form_actions.php
@@ -17,53 +17,63 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $formName = $_POST['formName'] ?? '';
     switch ($formName) {
         case 'login':
-            $userController = new userController();
-            try {
-                $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-                if (!$email) throw new \Exception('Doğru bir mail adresi girmelisiniz.');
-                $userController->login([
-                    'email' => $_POST['email'],
-                    'password' => $_POST['password'],
-                    "remember_me" => isset($_POST['remember_me'])
-                ]);
-            } catch (\Exception $e) {
-                $errors['login'] = $e->getMessage();
-            }
-
-            if (!count($errors) > 0)
-                header("location: /admin");
-            else {
-                $_SESSION['errors'] = $errors;
-                header("location: /admin/login.php");
-                exit();
-            }
-
+            handle_login();
             break;
         case 'register':
-            var_dump($_POST);
-            $new_user = new User();
-            if (!isset($_POST['terms'])) {
-                $errors[] = ['terms' => "Koşullar kabul edilmelidir"];
-            }
-            if ($_POST['password'] != $_POST['password_confirm']) {
-                $errors[] = ['password' => "Parolalar uyuşmuyor"];
-                $errors[] = ['password_confirm' => "Parolalar uyuşmuyor"];
-
-            }
-            //todo Kayıt işlemleri yapılacak
-            $new_user->email = $_POST['email'];
-            $new_user->name = $_POST['name'];
-            $new_user->lastname = $_POST['lastname'];
-            $new_user->password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-            var_dump($new_user);
-            if (!count($errors) > 0)
-                echo "";
-            //header("location: /admin/login.php");
-            else
-                var_dump($errors);
+            handle_register();
             break;
         default:
             echo "Form adı belirtilmemiş";
             break;
     }
+}
+
+function handle_login()
+{
+    global $errors;
+    $userController = new userController();
+    try {
+        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+        if (!$email) throw new \Exception('Doğru bir mail adresi girmelisiniz.');
+        $userController->login([
+            'email' => $_POST['email'],
+            'password' => $_POST['password'],
+            "remember_me" => isset($_POST['remember_me'])
+        ]);
+    } catch (\Exception $e) {
+        $errors['login'] = $e->getMessage();
+    }
+
+    if (!count($errors) > 0)
+        header("location: /admin");
+    else {
+        $_SESSION['errors'] = $errors;
+        header("location: /admin/login.php");
+        exit();
+    }
+}
+
+function handle_register()
+{
+    var_dump($_POST);
+    $new_user = new User();
+    if (!isset($_POST['terms'])) {
+        $errors[] = ['terms' => "Koşullar kabul edilmelidir"];
+    }
+    if ($_POST['password'] != $_POST['password_confirm']) {
+        $errors[] = ['password' => "Parolalar uyuşmuyor"];
+        $errors[] = ['password_confirm' => "Parolalar uyuşmuyor"];
+
+    }
+    //todo Kayıt işlemleri yapılacak
+    $new_user->email = $_POST['email'];
+    $new_user->name = $_POST['name'];
+    $new_user->lastname = $_POST['lastname'];
+    $new_user->password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    var_dump($new_user);
+    if (!count($errors) > 0)
+        echo "";
+    //header("location: /admin/login.php");
+    else
+        var_dump($errors);
 }
